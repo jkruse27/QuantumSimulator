a05e3ef1850e8ebd4b45c71d5829c8ba6cb38133
João Kruse <soyjog@gmail.com>
2022-03-07 21:32:31 -0300

    Correction on import paths

--- applications/grover/grover.py
+++ applications/grover/grover.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+import sys
+path = str(Path(Path(Path(__file__).parent.absolute()).parent.absolute().parent.absolute()))
+sys.path.insert(0, path)
+
 import scipy.sparse as sparse
 import numpy as np
 from quantum_simulator.simulator import Simulator
